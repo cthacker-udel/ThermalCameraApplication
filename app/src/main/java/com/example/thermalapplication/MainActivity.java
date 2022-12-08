3b81f1cea6eab9a40b6ae65ba5ea8630d6266815
IshwarKumarMunirathinamAnanthaSekaran <119716651+IshwarKumarMunirathinamAnanthaSekaran@users.noreply.github.com>
2022-12-07 19:27:31 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/thermalapplication/MainActivity.java
+++ app/src/main/java/com/example/thermalapplication/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is available.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
